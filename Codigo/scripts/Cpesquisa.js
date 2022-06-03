1eb5845b3a607162f8267d05b72fb9a390e63adb
Pedro Pampolini Mendicino <pedromendicino25@gmail.com>
2022-06-03 00:07:22 -0300

    Delete Codigo/scripts directory

--- Codigo/scripts/Cpesquisa.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#Cpesquisar').onclick = (evento) =>{
-  let variavel = Cmaterias.value;
-  variavel = removeAcento(variavel);
-  console.log(variavel);
-  location.href = `professores.html?search=${variavel}`;
-  evento.preventDefault();
-  //Clocalizacao
-  //Cmateria
-}
